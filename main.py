20ac18db91520a69977cf77486f5a934e503338a
Rogerio Stubs <rostubs17@gmail.com>
2021-04-24 16:23:35 -0300

    lista encadeada

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import hanoi
+from lista_encadeada import *
 
 if __name__ == '__main__':
-    hanoi.hanoi(2, 'A', 'B', 'C')
+    lista = ListaEncadeada()
+    lista.inserir_no_inicio(5)
+    print(lista)
+
+    lista.inserir_depois(lista.cabeca, 4)
+    print(lista)
+
+    lista.inserir_depois(lista.cabeca, 3)
+    print(lista)
